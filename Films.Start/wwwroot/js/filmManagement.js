bfbb2d208cff27772aeb1e36da66aa20141f7f90
Incendia <egor.lazeba@yandex.ru>
2024-04-29 03:02:51 +0300

    Убрал лишнее

--- Films.Start/wwwroot/js/filmManagement.js
+++
@@ -1,9 +0,0 @@
-function ParseData(elements, model) {
-    model.forEach(el => {
-        let html = '<div class="element col-lg-2 col-md-3 col-sm-4 col-6 text-center"><a href="FilmManagement/Edit?id=' + el.id + '" class="content-link"><img class="poster" src="/' + el.posterUri + '" alt=""><span>'+el.name+'</span></a></div>'
-        elements.append(html);
-    })
-}
-
-let scroller = new Scroller('.films', '#filter', ParseData)
-scroller.Start();
--- Films.Start/wwwroot/js/profile.js
+++
@@ -1,9 +0,0 @@
-function ParseData(elements, model) {
-    model.forEach(el => {
-        let html = '<div class="element col-lg-2 col-md-3 col-4 text-center"><a href="/Film?id=' + el.id + '" class="content-link"><img class="poster" src="/' + el.poster + '" alt=""><span>' + el.name + '</span></a><div>' + el.score + ' ★</div></div>'
-        elements.append(html);
-    })
-}
-
-let scroller = new Scroller('.ratings', '#filter', ParseData)
-scroller.Start();
